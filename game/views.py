8203c994af68cc1c691deeb6a79e87b932e56779
bzx <517291467@qq.com>
2023-06-01 00:50:45 +0800

    2023.6.1

---
+++ game/views.py
@@ -0,0 +1,4 @@
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("我的第一个网页")
--- webapp/settings.py
+++ webapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0-cuur2oyvarm(irkmq1@5!-31%_m4l9s*pt&8$hfzem4r^im(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["43.143.178.170"]
 
 
 # Application definition
