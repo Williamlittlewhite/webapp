ab62b8452e6ffbb8db6025d1e92c788b3e186ff5
william <517291467@qq.com>
2023-06-06 00:52:39 +0800

    2023.6.6debug

--- scripts/compress_game_js.sh
+++ scripts/compress_game_js.sh
@@ -4,4 +4,5 @@ JS_PATH=/home/bzx/webapp/game/static/js/
 JS_PATH_DIST=${JS_PATH}dist/
 JS_PATH_SRC=${JS_PATH}src/
 
-find $JS_PAHT_SRC -type f -name '*.js' | sort | xargs cat > ${JS_PATH_DIST}game.js
+find $JS_PATH_SRC -type f -name '*.js' | sort | xargs cat > ${JS_PATH_DIST}game.js
+
